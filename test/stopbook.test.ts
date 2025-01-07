367d6fb3cbf5ed2ce967965d2da475432a96da2c
fasenderos <fasenderos@gmail.com>
2025-01-07 15:29:05 +0100

    test: stopbook in snapshot

--- test/stopbook.test.ts
+++ test/stopbook.test.ts
@@ -144,3 +144,58 @@ void test("it should validate conditional order", () => {
 	validate(OrderType.STOP_MARKET, Side.SELL, null, 90, false, 90);
 	validate(OrderType.STOP_MARKET, Side.SELL, null, 90, false, 80);
 });
+
+void test("it should get snapshot", () => {
+	const ob = new StopBook();
+
+	const addOrder = (side: Side, orderId: string, stopPrice: number): void => {
+		const order = OrderFactory.createOrder({
+			id: orderId,
+			type: OrderType.STOP_LIMIT,
+			side,
+			size: 5,
+			price: stopPrice,
+			stopPrice,
+			timeInForce: TimeInForce.GTC,
+		});
+		ob.add(order);
+	};
+
+	//  Start with SELL side
+	addOrder(Side.SELL, "sell-1", 110);
+	addOrder(Side.SELL, "sell-2", 110); // Same price as before
+	addOrder(Side.SELL, "sell-3", 120);
+	addOrder(Side.SELL, "sell-4", 130);
+	addOrder(Side.SELL, "sell-5", 140);
+
+	// Test BUY side
+	addOrder(Side.BUY, "buy-1", 100);
+	addOrder(Side.BUY, "buy-2", 100); // Same price as before
+	addOrder(Side.BUY, "buy-3", 90);
+	addOrder(Side.BUY, "buy-4", 80);
+	addOrder(Side.BUY, "buy-5", 70);
+
+	const snapshot = ob.snapshot();
+	assert.equal(Array.isArray(snapshot.bids), true);
+	assert.equal(Array.isArray(snapshot.asks), true);
+	assert.equal(snapshot.bids.length, 4);
+	assert.equal(snapshot.asks.length, 4);
+
+	const price70 = snapshot.bids.find((x) => x.price === 70);
+	const price80 = snapshot.bids.find((x) => x.price === 80);
+	const price90 = snapshot.bids.find((x) => x.price === 90);
+	const price100 = snapshot.bids.find((x) => x.price === 100);
+	const price110 = snapshot.asks.find((x) => x.price === 110);
+	const price120 = snapshot.asks.find((x) => x.price === 120);
+	const price130 = snapshot.asks.find((x) => x.price === 130);
+	const price140 = snapshot.asks.find((x) => x.price === 140);
+
+	assert.equal(price70?.orders.length, 1);
+	assert.equal(price80?.orders.length, 1);
+	assert.equal(price90?.orders.length, 1);
+	assert.equal(price100?.orders.length, 2);
+	assert.equal(price110?.orders.length, 2);
+	assert.equal(price120?.orders.length, 1);
+	assert.equal(price130?.orders.length, 1);
+	assert.equal(price140?.orders.length, 1);
+});
--- test/stopqueue.test.ts
+++ test/stopqueue.test.ts
@@ -11,6 +11,7 @@ void test("it should append/remove orders from queue", () => {
 	// Test edge case where head is undefined (queue is empty)
 	assert.equal(oq.removeFromHead(), undefined);
 
+	// Test append()
 	const order1 = OrderFactory.createOrder({
 		type: OrderType.STOP_LIMIT,
 		id: "order1",
@@ -63,6 +64,14 @@ void test("it should append/remove orders from queue", () => {
 	oq.append(order4);
 	assert.equal(oq.len(), 4);
 
+	// Test toArray()
+	const orders = oq.toArray();
+	assert.deepStrictEqual(orders[0].toObject(), order1.toObject());
+	assert.deepStrictEqual(orders[1].toObject(), order2.toObject());
+	assert.deepStrictEqual(orders[2].toObject(), order3.toObject());
+	assert.deepStrictEqual(orders[3].toObject(), order4.toObject());
+
+	// Test remove()
 	assert.deepStrictEqual(oq.removeFromHead(), order1);
 	assert.deepStrictEqual(oq.remove(order4.id), order4);
 	assert.equal(oq.len(), 2);
