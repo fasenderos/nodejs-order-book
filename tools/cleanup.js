a6076272f33d91b5306c9f16cea0db70db81f77f
fasenderos <fasenderos@gmail.com>
2025-07-18 00:59:25 +0200

    chore: remove browser build

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -22,6 +22,5 @@ if (folder) {
 } else {
   deleteFolderRecursive(Path.join(__dirname, '../dist/cjs'))
   deleteFolderRecursive(Path.join(__dirname, '../dist/esm'))
-  deleteFolderRecursive(Path.join(__dirname, '../dist/umd'))
   deleteFolderRecursive(Path.join(__dirname, '../dist/types'))
 }
