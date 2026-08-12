afa676cc13cf8c9de6749d309ce2e9da9ac844ad
fasenderos <fasenderos@gmail.com>
2026-08-12 17:04:00 +0200

    chore: init migration to plugin system

---
+++ packages/core/benchmarks/benchmark_lob.js
@@ -0,0 +1,235 @@
+const bench = require("nanobench");
+const hft = require("../dist/cjs/index.js");
+const gaussian = require("gaussian");
+
+/* New Limits */
+function spamLimitOrders(book, count) {
+	for (let i = 0; i < count; i++) {
+		book.limit({ side: "buy", id: i.toString(), size: 50, price: i });
+	}
+}
+
+bench("Spam 1 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 1);
+	b.end();
+});
+
+bench("Spam 10 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 10);
+	b.end();
+});
+
+bench("Spam 100 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 100);
+	b.end();
+});
+
+bench("Spam 1000 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 1000);
+	b.end();
+});
+
+bench("Spam 100000 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 100000);
+	b.end();
+});
+
+bench("Spam 300000 new Limits", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitOrders(book, 300000);
+	b.end();
+});
+
+/* New Orders */
+function spamOrders(book, count, variance = 5) {
+	for (let i = 0; i < count; i++) {
+		book.limit({
+			side: "buy",
+			id: i.toString(),
+			size: 50,
+			price: i % variance,
+		});
+	}
+}
+
+bench("Spam 1 new Orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrders(book, 1);
+	b.end();
+});
+
+bench("Spam 10 new Orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrders(book, 10);
+	b.end();
+});
+
+bench("Spam 100 new Orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrders(book, 100);
+	b.end();
+});
+
+bench("Spam 1000 new Orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrders(book, 1000);
+	b.end();
+});
+
+/* Random submission and cancellation */
+function spamOrdersRandomCancels(
+	book,
+	count,
+	mean = 500,
+	variance = 30,
+	cancelEvery = 5,
+) {
+	const priceDistribution = gaussian(mean, variance);
+	book.limit({
+		side: "buy",
+		id: "0",
+		size: 50,
+		price: priceDistribution.ppf(Math.random()),
+	});
+	for (let i = 1; i < count; i++) {
+		book.limit({
+			side: "buy",
+			id: i.toString(),
+			size: 50,
+			price: priceDistribution.ppf(Math.random()),
+		});
+		if (i % cancelEvery === 0) book.cancel((i - cancelEvery).toString());
+	}
+}
+
+bench("Spam 10 orders and randomly cancel orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrdersRandomCancels(book, 10);
+	b.end();
+});
+
+bench("Spam 100 orders and randomly cancel orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrdersRandomCancels(book, 100);
+	b.end();
+});
+
+bench("Spam 1000 orders and randomly cancel orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrdersRandomCancels(book, 1000);
+	b.end();
+});
+
+bench("Spam 10000 orders and randomly cancel orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamOrdersRandomCancels(book, 10000);
+	b.end();
+});
+
+/* Random submission, cancellation, and market orders */
+function spamLimitRandomOrders(
+	book,
+	count,
+	priceMean = 500,
+	priceVariance = 20,
+	quantityMean = 100,
+	quantityVariance = 10,
+	orderEvery = 100,
+) {
+	const price = gaussian(priceMean, priceVariance);
+	const quantity = gaussian(quantityMean, quantityVariance);
+	for (let i = 1; i < count; i++) {
+		const price_ = price.ppf(Math.random());
+		const quantity_ = quantity.ppf(Math.random());
+		book.limit({ side: "buy", id: i.toString(), size: 100, price: price_ });
+		// random submit a market order
+		if (i % orderEvery === 0) book.market({ side: "sell", size: quantity_ });
+	}
+}
+
+bench("Spam 1000 limit orders and occasional market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitRandomOrders(book, 1000);
+	b.end();
+});
+
+bench("Spam 10000 limit orders and occasional market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitRandomOrders(book, 10000);
+	b.end();
+});
+
+bench("Spam 100000 limit orders and occasional market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitRandomOrders(book, 100000);
+	b.end();
+});
+
+function spamLimitManyMarketOrders(
+	book,
+	count,
+	priceMean = 500,
+	priceVariance = 20,
+	quantityMean = 50,
+	quantityVariance = 10,
+) {
+	const price = gaussian(priceMean, priceVariance);
+	const quantity = gaussian(quantityMean, quantityVariance);
+
+	for (let i = 1; i < count; i++) {
+		const price_ = price.ppf(Math.random());
+		const quantity_ = quantity.ppf(Math.random());
+		book.limit({ side: "buy", id: i.toString(), size: 100, price: price_ });
+		book.market({ side: "sell", size: quantity_ });
+	}
+}
+
+bench("Spam 10 limit orders and constant market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitManyMarketOrders(book, 10);
+	b.end();
+});
+
+bench("Spam 100 limit orders and constant market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitManyMarketOrders(book, 100);
+	b.end();
+});
+
+bench("Spam 1000 limit orders and constant market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitManyMarketOrders(book, 1000);
+	b.end();
+});
+
+bench("Spam 10000 limit orders and constant market orders", (b) => {
+	const book = new hft.OrderBook();
+	b.start();
+	spamLimitManyMarketOrders(book, 10000);
+	b.end();
+});
---
+++ packages/core/config/fileTransformer.js
@@ -0,0 +1,7 @@
+// source: https://jestjs.io/docs/code-transformation#examples
+const path = require("node:path");
+module.exports = {
+	process(_src, filename) {
+		return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+	},
+};
